32910eeb5e4c43b484b2660ce9109e04f4fe9d49
yowka <stasskirnevskiy@gmail.com>
2025-04-08 19:22:00 +0700

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::view('/welcome', 'welcome');
+Route::view('/about', 'about');
+Route::view('/poster', 'poster');
+Route::view('/contact', 'contact');
